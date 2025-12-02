8349376ab5a319650aac3751c8b071f4a66e0e4c
ShikharPatwa <shikharpatwa786@gmail.com>
2025-12-03 03:21:41 +0530

    design the Home page

---
+++ client/src/pages/Layout.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+const Layout = () => {
+  return (
+    <div>
+      <h1>
+        Layouts
+      </h1>
+      <Outlet/>
+    </div>
+  )
+}
+
+export default Layout
